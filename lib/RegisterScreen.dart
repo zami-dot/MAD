61f4fc9ad9a7916c7ce4e1712aee4d79962dd1c8
Aisha Channa <89415381+AishaChanna@users.noreply.github.com>
2023-09-24 00:22:11 +0500

    Add files via upload

--- lib/RegisterScreen.dart
+++ lib/RegisterScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:project/Login.dart'; // Ensure the path is correct
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.purple,
       ),
-      home: Login(),
+      home: RegisterScreen(),
     );
   }
 }
@@ -51,115 +51,125 @@ class _RegisterScreenState extends State<RegisterScreen> {
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  // Full Name TextField
-                  TextFormField(
-                    controller: _fullNameController,
-                    decoration: InputDecoration(
-                      hintText: "Full Name",
-                      fillColor: Colors.purple[100]!.withOpacity(0.5),
-                      filled: true,
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your full name';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-
-                  // Username TextField
-                  TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      hintText: "Username",
-                      fillColor: Colors.purple[100]!.withOpacity(0.5),
-                      filled: true,
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your username';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-
-                  // Email TextField
-                  TextFormField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      fillColor: Colors.purple[100]!.withOpacity(0.5),
-                      filled: true,
-                    ),
-                    validator: (value) {
-                      if (value == null ||
-                          value.isEmpty ||
-                          !value.contains('@')) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-
-                  // Password TextField
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      fillColor: Colors.purple[100]!.withOpacity(0.5),
-                      filled: true,
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length < 6) {
-                        return 'Password should be at least 6 characters long';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-
-                  // Confirm Password TextField
-                  TextFormField(
-                    controller: _confirmPasswordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Confirm Password",
-                      fillColor: Colors.purple[100]!.withOpacity(0.5),
-                      filled: true,
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please confirm your password';
-                      }
-                      if (value != _passwordController.text) {
-                        return 'Passwords do not match';
-                      }
-                      return null;
-                    },
-                  ),
                   SizedBox(height: 20),
-
-                  // Register Button
+                  _customTextField(
+                      hint: "Full Name",
+                      icon: Icons.person,
+                      controller: _fullNameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your full name';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 15),
+                  _customTextField(
+                      hint: "Username",
+                      icon: Icons.person_outline,
+                      controller: _usernameController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 15),
+                  _customTextField(
+                      hint: "Email",
+                      icon: Icons.email,
+                      controller: _emailController,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            !value.contains('@')) {
+                          return 'Please enter a valid Email';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 15),
+                  _customTextField(
+                      hint: "Password",
+                      icon: Icons.lock,
+                      obscureText: true,
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length < 6) {
+                          return 'Password should be at least 6 characters long';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 15),
+                  _customTextField(
+                      hint: "Confirm Password",
+                      icon: Icons.lock_outline,
+                      obscureText: true,
+                      controller: _confirmPasswordController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your password';
+                        }
+                        if (value != _passwordController.text) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      }),
+                  SizedBox(height: 25),
                   ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        // If all validations pass, perform registration (You can implement this)
                         print('Registration successful');
                       }
                     },
                     child: Text('Register'),
                     style: ElevatedButton.styleFrom(
                       primary: Colors.purple[600],
+                      shadowColor: Colors.purple[200],
+                      elevation: 5,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _customTextField({
+    required String hint,
+    required IconData icon,
+    TextEditingController? controller,
+    bool obscureText = false,
+    String? Function(String?)? validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      validator: validator,
+      decoration: InputDecoration(
+        hintText: hint,
+        prefixIcon: Icon(icon, color: Colors.purple[400]),
+        filled: true,
+        fillColor: Colors.purple[50],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Colors.purple[400]!),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Colors.red),
         ),
       ),
     );
