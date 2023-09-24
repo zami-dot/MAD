e1cd90896175575c34b3ee87eeb5bdeed70fb5a0
Aisha Channa <89415381+AishaChanna@users.noreply.github.com>
2023-09-24 18:08:40 +0500

    Add files via upload

---
+++ lib/game_over_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+import 'package:project/Login.dart'; // Ensure the path is correct
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // Set this to false
+
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Login(),
+    );
+  }
+}
+
+class GameOverScreen extends StatelessWidget {
+  final int score;
+  final int totalQuestions;
+
+  GameOverScreen(
+      {required this.score,
+      required this.totalQuestions,
+      required String name});
+
+  @override
+  Widget build(BuildContext context) {
+    double percentage = (score / totalQuestions) * 100;
+    String remarks;
+    if (percentage >= 80) {
+      remarks = "Excellent!";
+    } else if (percentage >= 60) {
+      remarks = "Very Good!";
+    } else if (percentage >= 40) {
+      remarks = "Good Effort!";
+    } else {
+      remarks = "Better Luck Next Time!";
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.purple[100],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Card(
+            elevation: 8,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Game Over",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Name: Aisha",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    "Your Score: $score/$totalQuestions",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    "Percentage: ${percentage.toStringAsFixed(2)}%",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    remarks,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: percentage >= 60 ? Colors.green : Colors.red,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacementNamed('/');
+                    },
+                    child: Text(
+                      "Play Again",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
